c5ac499b11d438058c8d34177dcf95ac4f8ce16d
xmrsale <xmrsale@protonmail.com>
2021-10-03 23:13:34 +1100

    Docker wallet file permission bug fix

--- xmrnode/start.sh
+++ xmrnode/start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 while true; do
-    if [[ -f ".bitmonero/wallet" ]]; then
+    ls -l ".bitmonero/wallet/"
+    if [[ -f ".bitmonero/wallet/wallet" ]]; then
         echo "Found wallet, booting monero-wallet-rpc..."
-        monero-wallet-rpc --rpc-bind-ip=0.0.0.0 --rpc-bind-port=18090 --daemon-host monerod --daemon-port 18089 --wallet-file .bitmonero/wallet --password '' --non-interactive
+        monero-wallet-rpc --rpc-bind-ip=0.0.0.0 --rpc-bind-port=18090 --daemon-host monerod --daemon-port 18089 --wallet-file .bitmonero/wallet/wallet --password '' --non-interactive
     else
         echo ".bitmonero/wallet not found.. Not starting Monero wallet rpc. Make sure you pass wallet file to docker.."
     fi
